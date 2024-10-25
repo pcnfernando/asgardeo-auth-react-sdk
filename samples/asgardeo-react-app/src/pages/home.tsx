d6f9d3f203b44877058257bd6906b1b0bfca21f0
Chiran Fernando <pcnfernando@gmail.com>
2024-10-25 14:33:43 +0530

    Remove authConfig validation

--- samples/asgardeo-react-app/src/pages/home.tsx
+++ samples/asgardeo-react-app/src/pages/home.tsx
@@ -117,19 +117,19 @@ export const HomePage: FunctionComponent = (): ReactElement => {
     };
 
     // If `clientID` is not defined in `config.json`, show a UI warning.
-    if (!authConfig?.clientID) {
+    // if (!authConfig?.clientID) {
 
-        return (
-            <div className="content">
-                <h2>You need to update the Client ID to proceed.</h2>
-                <p>Please open &quot;src/config.json&quot; file using an editor, and update
-                    the <code>clientID</code> value with the registered application&apos;s client ID.</p>
-                <p>Visit repo <a
-                    href="https://github.com/asgardeo/asgardeo-auth-react-sdk/tree/master/samples/asgardeo-react-app">README</a> for
-                    more details.</p>
-            </div>
-        );
-    }
+    //     return (
+    //         <div className="content">
+    //             <h2>You need to update the Client ID to proceed.</h2>
+    //             <p>Please open &quot;src/config.json&quot; file using an editor, and update
+    //                 the <code>clientID</code> value with the registered application&apos;s client ID.</p>
+    //             <p>Visit repo <a
+    //                 href="https://github.com/asgardeo/asgardeo-auth-react-sdk/tree/master/samples/asgardeo-react-app">README</a> for
+    //                 more details.</p>
+    //         </div>
+    //     );
+    // }
 
     if (hasLogoutFailureError) {
         return (
